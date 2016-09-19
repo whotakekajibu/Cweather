518306325ce61842c31ae7bdbdaee8da5e7917d1
Ning <572311320@qq.com>
2016-09-19 19:27:54 +0800

    First commit

--- app/src/main/java/com/example/ning/mycoolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/ning/mycoolweather/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.ning.mycoolweather;
+package com.example.ning.mycoolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.ning.mycoolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
